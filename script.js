1e6bdc6001fee9485246e094846325e73814d8c5
Xarchtech <154090445+Xarchtech@users.noreply.github.com>
2023-12-18 18:03:29 +0530

    Update script.js
    Countdown script

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Set the date we're counting down to (replace with your desired launch date)
+    const launchDate = new Date('April 10, 2024 00:00:00').getTime();
+
+    // Update the countdown every 1 second
+    const countdownInterval = setInterval(function () {
+        const now = new Date().getTime();
+        const distance = launchDate - now;
+
+        // Calculate days, hours, minutes, and seconds
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the countdown
+        document.getElementById('countdown').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+        // If the countdown is over, clear the interval
+        if (distance < 0) {
+            clearInterval(countdownInterval);
+            document.getElementById('countdown').innerHTML = 'Website is live!';
+        }
+    }, 1000);
+});
